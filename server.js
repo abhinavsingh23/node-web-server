2e407b6a237c7a75168ec31cb5d5c59698e362fc
abhinavsingh23 <abhinavsingh2902@gmail.com>
2020-05-27 00:00:25 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const hbs =require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs')
+// app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(err)=>{
+        if(err){
+            console.log('Unable to append to server.js');
+        }
+    });
+    next();
+});
+
+/********* Maintenance Mode***********************************/
+
+// app.use((req,res,next)=>{
+
+//     res.render('maintenance.hbs');
+
+// });
+/*************************************************************/
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+});
+
+app.get('/',(req,res)=>{
+
+    // res.send('<h1>Hello Express!</h1>');
+    // res.send({
+    //     name:'Abhinav',
+    //     likes: [
+    //         'Biking',
+    //         'Travel',
+    //     ]
+    // })
+
+    res.render('home.hbs',{
+            welcomeMessage: 'Welcome!!!',
+            pageTitle: 'Home',
+            // currentYear: new Date().getFullYear()
+    });
+
+
+
+
+});
+
+app.get('/about', (req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+        // currentYear : new  Date().getFullYear()
+    });
+});
+
+app.listen(3000, () =>{
+    console.log('Server is up on Port 3000');
+});
